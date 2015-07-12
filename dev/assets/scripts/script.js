ea97f775f625e7199dbc43030e1373f1e663fd55
afonsopacifer <afonsopacifer@live.com>
2015-07-11 21:30:19 -0300

    menu e favicons updated

--- dev/assets/scripts/script.js
+++ dev/assets/scripts/script.js
@@ -1,1 +1,36 @@
-console.log("run");
+(function() {
+  //Seleceiona os elementos e os atribuia a variaveis
+  var btnOpen = document.getElementById("btnOpen");
+  var menu = document.getElementById("menu");
+
+  //Função para exibir ou ocultar os itens do menu
+  function toggleMenu() {
+    if(menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
+
+  //Atribui a função ao evento click no botão abrir
+  btnOpen.addEventListener("click",toggleMenu);
+
+  //Cria a media querie
+  var mediaQuery = window.matchMedia("(min-width: 480px)");
+
+  //Verifica a media query e determina se os itens do menu deve aparecer ou não
+  if (mediaQuery.matches) {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+
+  //Verifica alterações e e determina se os itens do menu deve aparecer ou não
+  mediaQuery.addListener(function(changed) {
+    if(changed.matches) {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  });
+})();
