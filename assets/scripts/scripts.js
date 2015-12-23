5a69de1e713a4c6686b3bfc12f4a7d014344811d
afonsopacifer <afonsopolis@gmail.com>
2015-12-23 00:17:11 -0200

    Updates

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -34,3 +34,28 @@
     }
   });
 })();
+
+// Menu feedback
+$(document).ready(function(){
+
+  var navLink = $('.menu-link'),
+     local = window.location.pathname.split('/').pop();
+
+  menuFeedback();
+
+  function menuFeedback() {
+    navLink.each(function(){
+      if ($(this).attr('href') == local ) {
+        $(this).addClass('active');
+      } else {
+        $(this).removeClass('active');
+      }
+    });
+  };
+
+  if(local == "") {
+    console.log('dsd');
+    $('.menu-link:first').addClass('active');
+  }
+
+});
